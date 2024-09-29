5c41bdb39bb3f4f6bc49083f36890250eb67a849
AyaanWebDeveloper <developwithayaan@gmail.com>
2024-09-29 17:29:16 +0500

    Sweet alert

---
+++ app.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('load',function(){
+
+    // swal("Good job!", "This is Sweet Alert", "success");
+    swal("Good job!", "This is Sweet Alert", "success");
+      
+  
+})
+
+   
